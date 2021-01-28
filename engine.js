0398a3d32ce866af30403d0a2143e71452b754f3
a5zima <67965326+a5zima@users.noreply.github.com>
2021-01-28 17:55:45 +0200

    Add files via upload

---
+++ engine.js
@@ -0,0 +1,84 @@
+var data3 = [["", "centrs", 239, 266, 27, 282, 444, 52, 184, 714, 83], ["", "Vecm\u012blgr\u0101vis", 14, 172, 22, 11, 228, 45, 3, 360, 71], ["", "P\u013cavnieki", 45, 244, 37, 20, 308, 51, 12, 347, 64], ["", "Jugla", 19, 242, 34, 24, 297, 45, 5, 426, 77], ["", "Purvciems", 43, 228, 32, 48, 339, 48, 14, 360, 65], ["", "\u0136engarags", 40, 236, 31, 20, 270, 47, 6, 322, 60], ["", "Maskavas priek\u0161pils\u0113ta", 32, 202, 26, 19, 317, 42, 3, 550, 71], ["", "Sarkandaugava", 18, 194, 30, 18, 278, 48, 2, 275, 50], ["", "I\u013c\u0123uciems", 11, 216, 31, 19, 359, 50, 6, 337, 62], ["", "Ziepniekkalns", 25, 252, 34, 12, 324, 49, 6, 459, 79], ["", "Ber\u0123i", 1, 250, 25, 1, 190, 43, 0, 0, 0], ["", "\u0100genskalns", 41, 264, 26, 37, 404, 46, 18, 721, 88], ["", "\u010ciekurkalns", 15, 203, 29, 6, 348, 48, 2, 272, 40], ["", "Imanta", 19, 247, 31, 37, 329, 49, 14, 442, 84], ["", "Me\u017eciems", 10, 260, 32, 11, 351, 53, 3, 557, 83], ["", "Zolit\u016bde", 8, 278, 41, 10, 318, 58, 5, 406, 74], ["", "Katlakalns", 0, 0, 0, 1, 350, 46, 0, 0, 0], ["", "Manga\u013ci", 7, 199, 32, 3, 233, 40, 0, 0, 0], ["", "\u0160amp\u0113teris-Pleskod\u0101le", 11, 277, 31, 11, 357, 53, 14, 638, 91], ["", "Bolder\u0101ja", 8, 178, 28, 3, 233, 47, 1, 200, 66], ["", "Teika", 15, 252, 27, 25, 387, 45, 15, 526, 62], ["", "VEF", 0, 0, 0, 1, 320, 53, 0, 0, 0], ["", "Vecr\u012bga", 6, 408, 34, 22, 486, 53, 17, 760, 82], ["", "D\u0101rzciems", 2, 215, 37, 3, 350, 50, 4, 361, 71], ["", "Kleisti", 0, 0, 0, 0, 0, 0, 1, 490, 90], ["", "Krasta r-ns", 10, 210, 29, 4, 298, 45, 4, 329, 60], ["", "Kl\u012bversala", 3, 267, 30, 8, 398, 48, 5, 860, 105], ["", "Daugavgr\u012bva", 6, 171, 27, 3, 253, 53, 1, 250, 54], ["", "Cits", 2, 190, 26, 1, 425, 52, 0, 0, 0], ["", "Me\u017eaparks", 0, 0, 0, 8, 380, 52, 1, 350, 72], ["", "Dzegu\u017ekalns", 6, 248, 35, 8, 472, 49, 2, 550, 84], ["", "Gr\u012bzi\u0146kalns", 6, 281, 29, 3, 333, 45, 0, 0, 0], ["", "Tor\u0146akalns", 9, 205, 27, 4, 310, 43, 3, 402, 72], ["", "Dreili\u0146i", 1, 240, 32, 0, 0, 0, 0, 0, 0], ["", "\u0136\u012bpsala", 1, 395, 30, 1, 325, 66, 3, 1410, 97], ["", "Bieri\u0146i", 1, 350, 68, 1, 420, 42, 0, 0, 0], ["", "Vec\u0101\u0137i", 0, 0, 0, 0, 0, 0, 1, 700, 105], ["", "\u0160\u0137irotava", 0, 0, 0, 0, 0, 0, 0, 0, 0], ["", "Aplokciems", 1, 250, 27, 0, 0, 0, 0, 0, 0], ["", "Zasulauks", 1, 220, 50, 1, 260, 45, 0, 0, 0], ["", "Jaunciems", 0, 0, 0, 1, 270, 40, 0, 0, 0]]
+var data2 = [["Centrs", 239, "\u20ac266", "27m\u00b2", 282, "\u20ac444", "52m\u00b2", 184, "\u20ac714", "83m\u00b2"], ["Vecm\u012blgr\u0101vis", 14, "\u20ac172", "22m\u00b2", 11, "\u20ac228", "45m\u00b2", 3, "\u20ac360", "71m\u00b2"], ["P\u013cavnieki", 45, "\u20ac244", "37m\u00b2", 20, "\u20ac308", "51m\u00b2", 12, "\u20ac347", "64m\u00b2"], ["Jugla", 19, "\u20ac242", "34m\u00b2", 24, "\u20ac297", "45m\u00b2", 5, "\u20ac426", "77m\u00b2"], ["Purvciems", 43, "\u20ac228", "32m\u00b2", 48, "\u20ac339", "48m\u00b2", 14, "\u20ac360", "65m\u00b2"], ["\u0136engarags", 40, "\u20ac236", "31m\u00b2", 20, "\u20ac270", "47m\u00b2", 6, "\u20ac322", "60m\u00b2"], ["Maskavas priek\u0161pils\u0113ta", 32, "\u20ac202", "26m\u00b2", 19, "\u20ac317", "42m\u00b2", 3, "\u20ac550", "71m\u00b2"], ["Sarkandaugava", 18, "\u20ac194", "30m\u00b2", 18, "\u20ac278", "48m\u00b2", 2, "\u20ac275", "50m\u00b2"], ["I\u013c\u0123uciems", 11, "\u20ac216", "31m\u00b2", 19, "\u20ac359", "50m\u00b2", 6, "\u20ac337", "62m\u00b2"], ["Ziepniekkalns", 25, "\u20ac252", "34m\u00b2", 12, "\u20ac324", "49m\u00b2", 6, "\u20ac459", "79m\u00b2"], ["Ber\u0123i", 1, "\u20ac250", "25m\u00b2", 1, "\u20ac190", "43m\u00b2", 0, 0, 0], ["\u0100genskalns", 41, "\u20ac264", "26m\u00b2", 37, "\u20ac404", "46m\u00b2", 18, "\u20ac721", "88m\u00b2"], ["\u010ciekurkalns", 15, "\u20ac203", "29m\u00b2", 6, "\u20ac348", "48m\u00b2", 2, "\u20ac272", "40m\u00b2"], ["Imanta", 19, "\u20ac247", "31m\u00b2", 37, "\u20ac329", "49m\u00b2", 14, "\u20ac442", "84m\u00b2"], ["Me\u017eciems", 10, "\u20ac260", "32m\u00b2", 11, "\u20ac351", "53m\u00b2", 3, "\u20ac557", "83m\u00b2"], ["Zolit\u016bde", 8, "\u20ac278", "41m\u00b2", 10, "\u20ac318", "58m\u00b2", 5, "\u20ac406", "74m\u00b2"], ["Katlakalns", 0, 0, 0, 1, "\u20ac350", "46m\u00b2", 0, 0, 0], ["Manga\u013ci", 7, "\u20ac199", "32m\u00b2", 3, "\u20ac233", "40m\u00b2", 0, 0, 0], ["\u0160amp\u0113teris-Pleskod\u0101le", 11, "\u20ac277", "31m\u00b2", 11, "\u20ac357", "53m\u00b2", 14, "\u20ac638", "91m\u00b2"], ["Bolder\u0101ja", 8, "\u20ac178", "28m\u00b2", 3, "\u20ac233", "47m\u00b2", 1, "\u20ac200", "66m\u00b2"], ["Teika", 15, "\u20ac252", "27m\u00b2", 25, "\u20ac387", "45m\u00b2", 15, "\u20ac526", "62m\u00b2"], ["VEF", 0, 0, 0, 1, "\u20ac320", "53m\u00b2", 0, 0, 0], ["Vecr\u012bga", 6, "\u20ac408", "34m\u00b2", 22, "\u20ac486", "53m\u00b2", 17, "\u20ac760", "82m\u00b2"], ["D\u0101rzciems", 2, "\u20ac215", "37m\u00b2", 3, "\u20ac350", "50m\u00b2", 4, "\u20ac361", "71m\u00b2"], ["Kleisti", 0, 0, 0, 0, 0, 0, 1, "\u20ac490", "90m\u00b2"], ["Krasta r-ns", 10, "\u20ac210", "29m\u00b2", 4, "\u20ac298", "45m\u00b2", 4, "\u20ac329", "60m\u00b2"], ["Kl\u012bversala", 3, "\u20ac267", "30m\u00b2", 8, "\u20ac398", "48m\u00b2", 5, "\u20ac860", "105m\u00b2"], ["Daugavgr\u012bva", 6, "\u20ac171", "27m\u00b2", 3, "\u20ac253", "53m\u00b2", 1, "\u20ac250", "54m\u00b2"], ["Cits", 2, "\u20ac190", "26m\u00b2", 1, "\u20ac425", "52m\u00b2", 0, 0, 0], ["Me\u017eaparks", 0, 0, 0, 8, "\u20ac380", "52m\u00b2", 1, "\u20ac350", "72m\u00b2"], ["Dzegu\u017ekalns", 6, "\u20ac248", "35m\u00b2", 8, "\u20ac472", "49m\u00b2", 2, "\u20ac550", "84m\u00b2"], ["Gr\u012bzi\u0146kalns", 6, "\u20ac281", "29m\u00b2", 3, "\u20ac333", "45m\u00b2", 0, 0, 0], ["Tor\u0146akalns", 9, "\u20ac205", "27m\u00b2", 4, "\u20ac310", "43m\u00b2", 3, "\u20ac402", "72m\u00b2"], ["Dreili\u0146i", 1, "\u20ac240", "32m\u00b2", 0, 0, 0, 0, 0, 0], ["\u0136\u012bpsala", 1, "\u20ac395", "30m\u00b2", 1, "\u20ac325", "66m\u00b2", 3, "\u20ac1410", "97m\u00b2"], ["Bieri\u0146i", 1, "\u20ac350", "68m\u00b2", 1, "\u20ac420", "42m\u00b2", 0, 0, 0], ["Vec\u0101\u0137i", 0, 0, 0, 0, 0, 0, 1, "\u20ac700", "105m\u00b2"], ["\u0160\u0137irotava", 0, 0, 0, 0, 0, 0, 0, 0, 0], ["Aplokciems", 1, "\u20ac250", "27m\u00b2", 0, 0, 0, 0, 0, 0], ["Zasulauks", 1, "\u20ac220", "50m\u00b2", 1, "\u20ac260", "45m\u00b2", 0, 0, 0], ["Jaunciems", 0, 0, 0, 1, "\u20ac270", "40m\u00b2", 0, 0, 0]]
+var ctx = document.getElementById('myChart').getContext('2d');
+
+$(document).ready( function () {
+    $('#table_id').DataTable( {
+        data: data2,
+        "bLengthChange": false,
+        "bPaginate": false,
+        "bInfo": false,
+        "responsive": true,
+        "bFilter": false,
+
+    });
+} );
+
+// Chart
+
+// Chart #1 Offer correlation to price of 1-2-3 rooms apartments.
+
+
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+        labels: ['0-50', '51-100', '101-150', '151-200', '201-250', '251-300', '301-350', '351-400', '401-450', '451-500', '501-550', '551-600', '601-650', '651-700', '701-750', '751-800', '801-850', '851-900', '901-950', '951-1000'],
+        datasets: [{
+            label: '1 room',
+            backgroundColor: 'rgb(255,99,132, 0.2)',
+            borderColor: 'rgb(255,99,132)',
+            data: [0, 0, 18, 38, 52, 50, 40, 18, 13, 3, 4, 2, 0, 0, 0, 1, 0, 0, 0, 0]
+        },
+            {
+                label: '2 rooms',
+
+                backgroundColor: 'rgb(99,125,255, 0.2)',
+                borderColor: 'rgb(99,115,255)',
+                data: [0, 0, 0, 3, 6, 23, 39, 40, 40, 44, 20, 19, 16, 19, 2, 2, 5, 2, 0, 0]
+            },
+            {
+                label: '3 rooms',
+
+                backgroundColor: 'rgb(128,255,99, 0.2)',
+                borderColor: 'rgb(104,255,99)',
+                data: [0, 0, 0, 0, 0, 3, 6, 12, 8, 14, 7, 15, 18, 20, 5, 13, 13, 11, 3, 6]
+            },
+        ]
+    },
+
+    // Configuration options go here
+    options: {
+
+        title: {
+            display: true,
+            text: 'CENTRAL DISTRICT: OFFERS BY MONTH PRICE (EUR)'},
+        responsive: true
+
+    }
+});
+
+// Chart Pie
+
+var ctx2 = document.getElementById('myChart2').getContext('2d');
+var myDoughnutChart = new Chart(ctx2, {
+    type: 'doughnut',
+
+    data: {labels: ["Long-Term", "Short-Term"],
+
+        datasets: [{
+            label: ['1 room'],
+            backgroundColor: ["#3e95cd", "red", "#c45850", "#8e5ea2",,"#e8c3b9","#c45850", "#5056c4", "#50c469" ],
+            data: [1827, 102]
+        }]},
+    options: {
+        title: {
+            display: true,
+            text: 'OFFERS BY THE LEASE TERM'},
+        legend: {
+            position: 'bottom',
+        }
+    }
+});
